92c08b57ec06bf1228a593513007aefbee64fbb7
AanyaMsh <aanyam007@gmail.com>
2026-02-06 20:34:03 +0530

    q5.c

---
+++ q5.c
@@ -0,0 +1,43 @@
+// Arrange Arrival Logs
+// Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Create a single chronological log that preserves the correct order of arrivals.
+#include <stdio.h>
+
+int main() {
+    int p, q, i, j, k;
+
+    scanf("%d", &p);
+    int log1[p];
+    for(i = 0; i < p; i++) {
+        scanf("%d", &log1[i]);
+    }
+
+    scanf("%d", &q);
+    int log2[q];
+    for(i = 0; i < q; i++) {
+        scanf("%d", &log2[i]);
+    }
+
+    int merged[p + q];
+    i = j = k = 0;
+
+    while(i < p && j < q) {
+        if(log1[i] <= log2[j]) {
+            merged[k++] = log1[i++];
+        } else {
+            merged[k++] = log2[j++];
+        }
+    }
+
+    while(i < p) {
+        merged[k++] = log1[i++];
+    }
+    while(j < q) {
+        merged[k++] = log2[j++];
+    }
+
+    for(i = 0; i < p + q; i++) {
+        printf("%d ", merged[i]);
+    }
+
+    return 0;
+}
